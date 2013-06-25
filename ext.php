3f8576218c077d0ab3cd41071d5fb88c3b9b0d59
rechosen <rechosen@gmail.com>
2013-06-25 14:45:55 +0200

    Add ext.php to make the extension work.

---
+++ ext.php
@@ -0,0 +1,20 @@
+<?php
+/**
+*
+* @package phpBB Karma
+* @copyright (c) 2013 rechosen
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+exit;
+}
+
+class phpbb_ext_karma_ext extends phpbb_extension_base
+{
+}
