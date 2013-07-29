9288c57434ae06428fd4630c9b71ad86b6021fa9
rechosen <rechosen@gmail.com>
2013-07-29 15:48:50 +0200

    First step in working towards a generic karma system: update the migrations file.

--- migrations/0_0_1.php
+++ migrations/0_0_1.php
@@ -30,16 +30,25 @@ class phpbb_ext_phpbb_karma_migrations_0_0_1 extends phpbb_db_migration
 	{
 		$ret = array(
 			'add_tables'	=> array(
-				$this->table_prefix . 'karma'	=> array(
+				$this->table_prefix . 'karma'		=> array(
 					'COLUMNS'		=> array(
-						'post_id'			=> array('UINT', 0),
-						'giving_user_id'	=> array('UINT', 0),
-						'receiving_user_id'	=> array('UINT', 0),
-						'karma_score'		=> array('TINT:4', 0),
-						'karma_time'		=> array('UINT:11', 0),
-						'karma_comment'		=> array('TEXT_UNI', ''),
+						'item_id'				=> array('UINT', 0),
+						'karma_type_id'			=> array('UINT', 0),
+						'giving_user_id'		=> array('UINT', 0),
+						'receiving_user_id'		=> array('UINT', 0),
+						'karma_score'			=> array('TINT:4', 0),
+						'karma_time'			=> array('TIMESTAMP', 0),
+						'karma_comment'			=> array('TEXT_UNI', ''),
 					),
-					'PRIMARY_KEY'	=> array('post_id', 'giving_user_id'),
+					'PRIMARY_KEY'	=> array('item_id', 'karma_type_id', 'giving_user_id'),
+				),
+				$this->table_prefix . 'karma_type'	=> array(
+					'COLUMNS'		=> array(
+						'karma_type_id'			=> array('UINT', 0),
+						'karma_type_name'		=> array('VCHAR:255', ''),
+						'karma_type_enabled'	=> array('BOOL', 0),
+					),
+					'PRIMARY_KEY'	=> 'karma_type_id',
 				),
 			),
 			'add_columns'	=> array(
