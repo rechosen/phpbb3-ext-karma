aba19842f776cd9cb3678660bf3946815565877f
rechosen <rechosen@gmail.com>
2013-08-09 16:16:00 +0200

    Added an event listener for adding user_karma_score to ucp_pm_viewmessage, and made event listener and template variable names more specific.

--- event/main_listener.php
+++ event/main_listener.php
@@ -25,19 +25,20 @@ class phpbb_ext_phpbb_karma_event_main_listener implements EventSubscriberInterf
 	static public function getSubscribedEvents()
 	{
 		return array(
-			'core.viewtopic_cache_user_data' => 'add_karma_score_to_user_cache_data',
-			'core.viewtopic_modify_post_row' => 'add_postrow_user_karma_score',
+			'core.viewtopic_cache_user_data'	=> 'viewtopic_body_add_karma_score_to_user_cache_data',
+			'core.viewtopic_modify_post_row'	=> 'viewtopic_body_add_postrow_user_karma_score',
+			'core.ucp_pm_view_messsage'			=> 'ucp_pm_viewmessage_add_pm_author_karma_score',
 		);
 	}
 
-	public function add_karma_score_to_user_cache_data($event)
+	public function viewtopic_body_add_karma_score_to_user_cache_data($event)
 	{
 		$user_cache_data = $event['user_cache_data'];
 		$user_cache_data['karma_score'] = $event['row']['user_karma_score'];
 		$event['user_cache_data'] = $user_cache_data;
 	}
 
-	public function add_postrow_user_karma_score($event)
+	public function viewtopic_body_add_postrow_user_karma_score($event)
 	{
 		global $user;
 
@@ -47,8 +48,20 @@ class phpbb_ext_phpbb_karma_event_main_listener implements EventSubscriberInterf
 			$user->add_lang_ext('phpbb/karma', 'karma');
 
 			$post_row = $event['post_row'];
-			$post_row['POSTER_KARMA'] = $event['user_poster_data']['karma_score'];
+			$post_row['POSTER_KARMA_SCORE'] = $event['user_poster_data']['karma_score'];
 			$event['post_row'] = $post_row;
 		}
+	}
+
+	public function ucp_pm_viewmessage_add_pm_author_karma_score($event)
+	{
+		global $user;
+
+		// Load the karma language file
+		$user->add_lang_ext('phpbb/karma', 'karma');
+
+		$msg_data = $event['msg_data'];
+		$msg_data['AUTHOR_KARMA_SCORE'] = $event['message_row']['user_karma_score'];
+		$event['msg_data'] = $msg_data;
 	}
 }
