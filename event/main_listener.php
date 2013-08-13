8ef511dd98e35a1c6b2a73e787135a0a390fe6d2
rechosen <rechosen@gmail.com>
2013-08-13 14:35:20 +0200

    Added the karma score to the user statistics when viewing a member profile.

--- event/main_listener.php
+++ event/main_listener.php
@@ -25,9 +25,10 @@ class phpbb_ext_phpbb_karma_event_main_listener implements EventSubscriberInterf
 	static public function getSubscribedEvents()
 	{
 		return array(
-			'core.viewtopic_cache_user_data'	=> 'viewtopic_body_add_karma_score_to_user_cache_data',
-			'core.viewtopic_modify_post_row'	=> 'viewtopic_body_add_postrow_user_karma_score',
-			'core.ucp_pm_view_messsage'			=> 'ucp_pm_viewmessage_add_pm_author_karma_score',
+			'core.viewtopic_cache_user_data'		=> 'viewtopic_body_add_karma_score_to_user_cache_data',
+			'core.viewtopic_modify_post_row'		=> 'viewtopic_body_add_postrow_user_karma_score',
+			'core.ucp_pm_view_messsage'				=> 'ucp_pm_viewmessage_add_pm_author_karma_score',
+			'core.memberlist_prepare_profile_data'	=> 'memberlist_view_add_karma_score_to_user_statistics',
 		);
 	}
 
@@ -63,5 +64,17 @@ class phpbb_ext_phpbb_karma_event_main_listener implements EventSubscriberInterf
 		$msg_data = $event['msg_data'];
 		$msg_data['AUTHOR_KARMA_SCORE'] = $event['message_row']['user_karma_score'];
 		$event['msg_data'] = $msg_data;
+	}
+
+	public function memberlist_view_add_karma_score_to_user_statistics($event)
+	{
+		global $user;
+
+		// Load the karma language file
+		$user->add_lang_ext('phpbb/karma', 'karma');
+
+		$template_data = $event['template_data'];
+		$template_data['USER_KARMA_SCORE'] = $event['data']['user_karma_score'];
+		$event['template_data'] = $template_data;
 	}
 }
